e94c8c8b9fd48d29904aff872f75a6ea8d8febb6
Asif Shaikh <ripclaw_ffb@hotmail.com>
2019-11-08 04:44:18 +0000

    Initial Release

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+﻿# P4V Chocolatey Package
+
+Helix Visual Client from Perforce boosts productivity
+
+Helix Visual Client (P4V) comes with Helix Core –– version control from Perforce. It allows developers to:
+
+* Easily customize their workspace.
+* Visualize the flow of change using Perforce Streams.
+* Initiate code reviews.
+* Submit files back into Helix Core.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'p4v'
+$url32 = 'https://cdist2.perforce.com/perforce/r19.2/bin.ntx86/p4vinst.exes'
+$url64 = 'https://cdist2.perforce.com/perforce/r19.2/bin.ntx64/p4vinst64.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  installerType	 = 'EXE'
+  url            = $url32
+  url64Bit       = $url64
+  silentArgs	 = '/s /v"/qn"'
+}
+
+Install-ChocolateyPackage @packageArgs
