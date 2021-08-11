f11af13a560e394973b60972105d49fa16ba1b0e
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-08-10 19:42:46 -0700

    Remove x86 support

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,15 @@
 ﻿$packageName = 'p4v'
 $version = 'r21.2'
 $baseurl = "https://cdist2.perforce.com/perforce/$version"
-$url32 = "$baseurl/bin.ntx86/p4vinst.exe"
-$url64 = "$baseurl/bin.ntx64/p4vinst64.exe"
-$checksum32 = ((Invoke-WebRequest "$baseurl/bin.ntx86/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'p4vinst.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
+$url = "$baseurl/bin.ntx64/p4vinst64.exe"
 $checksum64 = ((Invoke-WebRequest "$baseurl/bin.ntx64/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'p4vinst64.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
 
 $packageArgs = @{
   packageName    = $packageName
   installerType	 = 'EXE'
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
+  url            = $url
+  checksum       = $checksum64
   checksumType   = 'sha256'
-  checksumType64 = 'sha256'
   silentArgs	 = '/s /v"/qn"'
 }
 
