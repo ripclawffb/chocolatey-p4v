26f0dda857894dd5cf51f06bb68355a494b219b7
Asif Shaikh <ripclaw_ffb@hotmail.com>
2024-04-18 06:41:40 -0700

    Change URL to filehost

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'p4v'
-$version = 'r23.4'
-$baseurl = "https://cdist2.perforce.com/perforce/$version"
+$version = 'r24.1'
+$baseurl = "https://filehost.perforce.com/perforce/$version"
 $url = "$baseurl/bin.ntx64/p4vinst64.exe"
 # $checksum64 = ((Invoke-WebRequest "$baseurl/bin.ntx64/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'p4vinst64.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
 $checksum64 = 'e40616c2852271f26294fbf3e16a880dcba4b1850ab4418df685e861ffe4e0c8'
