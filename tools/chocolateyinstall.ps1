dff55dda215b1bc82b0a4af4a125846d4b59ed9c
ripclawffb <20031994+ripclawffb@users.noreply.github.com>
2026-08-07 08:45:25 +0000

    Update P4V to 2026.2.3029854

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $version = 'r26.2'
 $baseurl = "https://filehost.perforce.com/perforce/$version"
 $url = "$baseurl/bin.ntx64/p4vinst64.exe"
 # $checksum64 = ((Invoke-WebRequest "$baseurl/bin.ntx64/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'p4vinst64.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
-$checksum64 = '80c592bd948ffe22799d005302eca50dd5780e80f7c08f3b54436c3867c9b64d'
+$checksum64 = '24e147557a4ac4092b2e80c1e182b3a640c5fe662ecc8ab95d468c323a789c28'
 
 $packageArgs = @{
   packageName    = $packageName
